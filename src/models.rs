d5b473f202893313bcca0219c72cbb018c2e4dcc
kurlendapawel <kurlendapawel@gmail.com>
2024-03-06 19:48:22 +0100

    add chrono, fix cache Put

--- src/models.rs
+++ src/models.rs
@@ -6,13 +6,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct NewCache {
     pub value: String,
-    pub expiration_datetime: OffsetDateTime
+    pub expiration_datetime: chrono::DateTime<chrono::Utc>
 }
 impl From<web::Json<NewCache>> for NewCache {
-    fn from(cache: web::Json<NewCache>) -> Self {
+    fn from(tweet: web::Json<NewCache>) -> Self {
         NewCache {
-            value: cache.0.value,
-            expiration_datetime: cache.0.expiration_datetime,
+            value: tweet.0.value,
+            expiration_datetime: tweet.0.expiration_datetime,
         }
     }
 }
