73b3d53c20b3f4ab332d58e74b6e4b4aec226f15
kurlendapawel <kurlendapawel@gmail.com>
2024-03-13 20:31:28 +0100

    init cache

---
+++ server/src/cache.rs
@@ -0,0 +1,14 @@
+
+pub struct CacheModel {
+    key: String,
+    value: String,
+    pub expiration_datetime: chrono::DateTime<chrono::Utc>
+}
+
+pub fn add_or_update() {
+
+}
+
+pub fn delete(key: &str) {
+
+}
