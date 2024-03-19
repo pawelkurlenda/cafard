e19a25e7845dda7465ad0ff72ad68eae5f57da07
kurlendapawel <kurlendapawel@gmail.com>
2024-03-19 06:14:23 +0100

    Add myJob for background job

--- server/src/background_job.rs
+++ server/src/background_job.rs
@@ -2,3 +2,9 @@
 pub struct MyState {
     pub app_name: String,
 }
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+pub struct MyJob {
+    some_usize: usize,
+    other_usize: usize,
+}
