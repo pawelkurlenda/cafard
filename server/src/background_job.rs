a5fe4699b3c3258f94cbf6716a3cbfdcb598f96d
kurlendapawel <kurlendapawel@gmail.com>
2024-03-21 08:18:16 +0100

    add backroundjobs myjob job impl

--- server/src/background_job.rs
+++ server/src/background_job.rs
@@ -1,3 +1,8 @@
+use std::future::{ready, Ready};
+use background_jobs_core::{Backoff, Job, MaxRetries, BoxError};
+
+pub const DEFAULT_QUEUE: &'static str = "default";
+
 #[derive(Clone, Debug)]
 pub struct MyState {
     pub app_name: String,
@@ -23,5 +28,48 @@ impl MyJob {
             some_usize,
             other_usize,
         }
+    }
+}
+
+impl Job for MyJob {
+    type State = MyState;
+    type Error = BoxError;
+    type Future = Ready<Result<(), BoxError>>;
+
+    // The name of the job. It is super important that each job has a unique name,
+    // because otherwise one job will overwrite another job when they're being
+    // registered.
+    const NAME: &'static str = "MyJob";
+
+    // The queue that this processor belongs to
+    //
+    // Workers have the option to subscribe to specific queues, so this is important to
+    // determine which worker will call the processor
+    //
+    // Jobs can optionally override the queue they're spawned on
+    const QUEUE: &'static str = DEFAULT_QUEUE;
+
+    // The number of times background-jobs should try to retry a job before giving up
+    //
+    // This value defaults to MaxRetries::Count(5)
+    // Jobs can optionally override this value
+    const MAX_RETRIES: MaxRetries = MaxRetries::Count(1);
+
+    // The logic to determine how often to retry this job if it fails
+    //
+    // This value defaults to Backoff::Exponential(2)
+    // Jobs can optionally override this value
+    const BACKOFF: Backoff = Backoff::Exponential(2);
+
+    // This is important for allowing the job server to reap processes that were started but never
+    // completed.
+    //
+    // Defaults to 5 seconds
+    const HEARTBEAT_INTERVAL: u64 = 5_000;
+
+    fn run(self, state: MyState) -> Self::Future {
+        println!("{}: args, {:?}", state.app_name, self);
+
+        ready(Ok(()))
     }
 }
