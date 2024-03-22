1671d38faabc98b7e016df3ab2deaef73edb3e0e
kurlendapawel <kurlendapawel@gmail.com>
2024-03-22 08:46:43 +0100

    add backroundjobs invoke in main

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,9 +9,14 @@ use dotenv::dotenv;
 use std::env;
 use std::io;
 use std::sync::Mutex;
+use std::time::Duration;
 use actix_web::{App, HttpServer, web};
+use background_jobs_actix::{ActixTimer, WorkerConfig};
+use crate::background_job::{DEFAULT_QUEUE, MyJob, MyState};
 use crate::routes::{cache_routes, general_routes};
-use crate::state::AppState;
+use crate::state::{AppState, CacheState};
+
+//const DEFAULT_QUEUE: &'static str = "default";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -19,9 +24,21 @@ async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
 
     let shared_data = web::Data::new(AppState {
-       health_check_response: "OK".to_string(),
-       visit_count: Mutex::new(0),
+        health_check_response: "OK".to_string(),
+        visit_count: Mutex::new(0),
     });
+
+    use background_jobs_core::memory_storage::Storage;
+    let storage = Storage::new(ActixTimer);
+
+    let queue_handle = WorkerConfig::new(storage, move |_| MyState::new("My App"))
+        .register::<MyJob>()
+        .set_worker_count(DEFAULT_QUEUE, 16)
+        .start();
+
+    //queue_handle.queue(MyJob::new(1, 2)).await.unwrap_or_default();
+    queue_handle.every(Duration::from_secs(10) , MyJob::new(3, 4)).unwrap_or_default();
+    //queue_handle.schedule(MyJob::new(5, 6)).await.unwrap_or_default();
 
     let app = move || {
         App::new()
