f3d19344a1ef231a1509c01190994ba9f7fc6b2a
kurlendapawel <kurlendapawel@gmail.com>
2024-03-27 18:54:02 +0100

    temporary disable background job

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,7 +12,7 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use actix_web::{App, HttpServer, web};
 use background_jobs_actix::{ActixTimer, WorkerConfig};
-use crate::background_job::{DEFAULT_QUEUE, MyJob, MyState};
+use crate::background_job::{DEFAULT_QUEUE, MyJob};
 use crate::cache::Cache;
 use crate::routes::{cache_routes, general_routes};
 use crate::state::{AppState, CacheState};
@@ -34,17 +34,17 @@ async fn main() -> std::io::Result<()> {
         cache: Cache::new()
     });
 
-    use background_jobs_core::memory_storage::Storage;
-    let storage = Storage::new(ActixTimer);
+    //use background_jobs_core::memory_storage::Storage;
+    //let storage = Storage::new(ActixTimer);
 
-    let queue_handle = WorkerConfig::new(storage, move |_| MyState::new("My App"))
-        .register::<MyJob>()
-        .set_worker_count(DEFAULT_QUEUE, 16)
-        .start();
+    //let queue_handle = WorkerConfig::new(storage, move |_| cache_shared_data.clone())
+    //    .register::<MyJob>()
+    //    .set_worker_count(DEFAULT_QUEUE, 16)
+    //    .start();
 
-    //queue_handle.queue(MyJob::new(1, 2)).await.unwrap_or_default();
-    queue_handle.every(Duration::from_secs(10) , MyJob::new(3, 4)).unwrap_or_default();
-    //queue_handle.schedule(MyJob::new(5, 6)).await.unwrap_or_default();
+    ////queue_handle.queue(MyJob::new(1, 2)).await.unwrap_or_default();
+    //queue_handle.every(Duration::from_secs(10) , MyJob::new()).unwrap_or_default();
+    ////queue_handle.schedule(MyJob::new(5, 6)).await.unwrap_or_default();
 
     let app = move || {
         App::new()
