fd1f485570b8abda010fedfd185cb27731db422a
kurlendapawel <kurlendapawel@gmail.com>
2024-03-30 20:13:01 +0100

    add upsert cache request validation

--- server/src/models.rs
+++ server/src/models.rs
@@ -1,13 +1,19 @@
-use std::time::Duration;
-use actix_web::cookie::time::{ OffsetDateTime};
 use actix_web::web;
+use chrono::{ DateTime, Utc };
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
+use validator::{Validate, ValidateArgs, ValidationError};
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Validate, Debug, Clone)]
 pub struct NewCache {
+    #[validate(required)]
     pub value: String,
-    pub expiration_datetime: chrono::DateTime<chrono::Utc>
+
+    #[validate(required)]
+    #[validate(custom(function = "validate", arg = "(i64, i64)"))]
+    pub expiration_datetime: DateTime<Utc>
 }
+
 impl From<web::Json<NewCache>> for NewCache {
     fn from(tweet: web::Json<NewCache>) -> Self {
         NewCache {
@@ -15,4 +21,16 @@ impl From<web::Json<NewCache>> for NewCache {
             expiration_datetime: tweet.0.expiration_datetime,
         }
     }
+}
+
+fn validate(expiration_datetime: DateTime<Utc>) -> Result<(), ValidationError> {
+    if expiration_datetime > Utc::now() {
+        Ok(())
+    } else {
+        Err(ValidationError::new("Expiration datetime validation failed"))
+    }
+}
+
+lazy_static! {
+
 }
