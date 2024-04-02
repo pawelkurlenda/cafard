21c3eb57832fe72c02fb0f08da5ae69b45097855
kurlendapawel <kurlendapawel@gmail.com>
2024-04-02 21:38:21 +0200

    change handlers to implement Responder

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,9 +1,9 @@
 use super::state::{AppState, CacheState};
-use actix_web::{HttpResponse, web};
+use actix_web::{HttpResponse, Responder, web};
 use validator::Validate;
 use crate::models::NewCache;
 
-pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
+pub async fn health_check_handler(app_state: web::Data<AppState>) -> impl Responder {
     let mut visit_count = app_state.visit_count.lock().unwrap();
     *visit_count += 1;
 
@@ -12,19 +12,19 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
 }
 
 #[allow(unused_variables)]
-pub async fn cache_keys_get_handler(cache_state: web::Data<CacheState>) -> HttpResponse {
+pub async fn cache_keys_get_handler(cache_state: web::Data<CacheState>) -> impl Responder {
     let cache_keys = vec!["test_key_1", "test_key_1"];
     HttpResponse::Ok().json(cache_keys)
 }
 
-pub async fn cache_get_handler(params: web::Path<String>, cache_state: web::Data<CacheState>) -> HttpResponse {
+pub async fn cache_get_handler(params: web::Path<String>, cache_state: web::Data<CacheState>) -> impl Responder {
     let cache_key = params.to_string();
     let cache_value = cache_state.cache.get(&cache_key);
 
     HttpResponse::Ok().json(cache_value)
 }
 
-pub async fn cache_put_handler(params: web::Path<String>, new_cache: web::Json<NewCache>, cache_state: web::Data<CacheState>) -> HttpResponse {
+pub async fn cache_put_handler(params: web::Path<String>, new_cache: web::Json<NewCache>, cache_state: web::Data<CacheState>) -> impl Responder {
     let cache_key = params.to_string();
     let is_valid = new_cache.validate();
     match is_valid {
@@ -36,7 +36,7 @@ pub async fn cache_put_handler(params: web::Path<String>, new_cache: web::Json<N
     }
 }
 
-pub async fn cache_delete_handler(params: web::Path<String>, cache_state: web::Data<CacheState>) -> HttpResponse {
+pub async fn cache_delete_handler(params: web::Path<String>, cache_state: web::Data<CacheState>) -> impl Responder {
     let cache_key = params.to_string();
     cache_state.cache.delete(&cache_key);
 
