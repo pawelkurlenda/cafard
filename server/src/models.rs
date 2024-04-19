b8ba159ae11e9df8a52a76610b7e0c0fd1138f4d
kurlendapawel <kurlendapawel@gmail.com>
2024-04-19 20:42:44 +0200

    cache - expire_time nullable - fix validate_expiration_datetime

--- server/src/models.rs
+++ server/src/models.rs
@@ -7,15 +7,12 @@ pub struct NewCache {
     pub value: String,
 
     #[validate(custom(function = "validate_expiration_datetime"))]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub expiration_datetime: Option<DateTime<Utc>>
 }
 
-fn validate_expiration_datetime(expiration_datetime: &Option<DateTime<Utc>>) -> Result<(), ValidationError> {
-    if expiration_datetime == None {
-        Ok(())
-    }
-
-    if expiration_datetime > &Some(Utc::now()) {
+fn validate_expiration_datetime(expiration_datetime: &DateTime<Utc>) -> Result<(), ValidationError> {
+    if expiration_datetime > &Utc::now() {
         Ok(())
     } else {
         Err(ValidationError::new("Expiration datetime validation failed"))
