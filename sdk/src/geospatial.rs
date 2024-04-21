f35ab80da0b3e978d145b3d9b86ea418f6283384
kurlendapawel <kurlendapawel@gmail.com>
2024-04-21 21:09:03 +0200

    add geo crates

--- sdk/src/geospatial.rs
+++ sdk/src/geospatial.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use geo::{HaversineDistance, Point, point};
 
-#[derive(Clone, Debug)]
-pub struct GeospatialItem {
-    longitude: f32,
-    latitude: f32,
-}
+/*#[derive(Clone, Debug)]
+pub struct GeospatialPoint {
+    longitude: f64,
+    latitude: f64,
+}*/
 
 #[derive(Debug)]
 pub struct Geospatial {
-    items: Mutex<HashMap<String, GeospatialItem>>,
+    items: Mutex<HashMap<String, Point<f64>>>,
 }
 
 impl Geospatial {
@@ -17,6 +18,24 @@ impl Geospatial {
         Arc::new(Geospatial {
             items: Mutex::new(HashMap::new()),
         })
+    }
+
+    //pub fn upsert_location(&self, key: String, longitude: f64, latitude: f64) {
+    pub fn upsert_location(&self, key: String, point: Point) {
+        let mut items = self.items.lock().unwrap();
+
+        items.insert(key, point);
+    }
+
+    pub fn get_nearby_locations(&self, point: Point, radius_in_meters: i32) {
+        let mut items = self.items.lock().unwrap();
+
+        let nearby_locations: Vec<(String, Point<f64>)> = items
+            .iter()
+            .filter(|(_, point)| point.haversine_distance(point) <= radius_in_meters * 1000.0)
+            .map(|(name, point)| (name.clone(), *point))
+            .collect();
+
     }
 
 }
