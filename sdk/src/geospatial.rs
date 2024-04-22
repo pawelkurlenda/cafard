4997aa2fe2a3798c3b5678f1462a64043527c718
kurlendapawel <kurlendapawel@gmail.com>
2024-04-22 20:13:58 +0200

    add get_location_by_id

--- sdk/src/geospatial.rs
+++ sdk/src/geospatial.rs
@@ -1,12 +1,6 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use geo::{HaversineDistance, Point, point};
-
-/*#[derive(Clone, Debug)]
-pub struct GeospatialPoint {
-    longitude: f64,
-    latitude: f64,
-}*/
+use geo::{HaversineDistance, Point};
 
 #[derive(Debug)]
 pub struct Geospatial {
@@ -20,24 +14,34 @@ impl Geospatial {
         })
     }
 
-    //pub fn upsert_location(&self, key: String, longitude: f64, latitude: f64) {
-    pub fn upsert_location(&self, key: String, point: Point) {
+    pub fn upsert_location(&self, key: String, point: Point<f64>) {
         let mut items = self.items.lock().unwrap();
 
         items.insert(key, point);
     }
 
-    pub fn get_nearby_locations(&self, point: Point, radius_in_meters: i32) {
+    pub fn get_nearby_locations(&self, central_point: &Point, radius_in_meters: i32) -> Vec<(String, Point<f64>)> {
         let mut items = self.items.lock().unwrap();
 
         let radius = radius_in_meters as f64 * 1000.0;
 
         let nearby_locations: Vec<(String, Point<f64>)> = items
             .iter()
-            .filter(|(_, point)| point.haversine_distance(point) <= radius)
+            .filter(|(_, point)| central_point.haversine_distance(point) <= radius)
             .map(|(name, point)| (name.clone(), *point))
             .collect();
 
+        nearby_locations
+    }
+
+    pub fn get_location_by_id(&self, key: &str) -> Option<Point<f64>> {
+        let mut items = self.items.lock().unwrap();
+
+        if let Some(item) = items.get(key) {
+            Some(item.clone())
+        } else {
+            None
+        }
     }
 
 }
