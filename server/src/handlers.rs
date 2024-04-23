8aa0caae36a2986d969020624fb480a8954aa2cb
kurlendapawel <kurlendapawel@gmail.com>
2024-04-23 07:47:28 +0200

    rename lock handlers

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -43,27 +43,35 @@ pub async fn cache_delete_handler(params: web::Path<String>, cache_state: web::D
     HttpResponse::Ok().finish()
 }
 
-pub async fn lock_acquire(params: web::Path<String>, lock_state: web::Data<LockState>) -> impl Responder {
+pub async fn lock_acquire_handler(params: web::Path<String>, lock_state: web::Data<LockState>) -> impl Responder {
     let lock_key = params.to_string();
     let is_acquired = lock_state.lock.try_acquire(&lock_key);
 
     HttpResponse::Ok().json(is_acquired)
 }
 
-pub async fn lock_release(params: web::Path<String>, lock_state: web::Data<LockState>) -> impl Responder {
+pub async fn lock_release_handler(params: web::Path<String>, lock_state: web::Data<LockState>) -> impl Responder {
     let lock_key = params.to_string();
     let is_released = lock_state.lock.try_release(&lock_key);
 
     HttpResponse::Ok().json(is_released)
 }
 
-pub async fn lock_status(params: web::Path<String>, lock_state: web::Data<LockState>) -> impl Responder {
+pub async fn lock_status_handler(params: web::Path<String>, lock_state: web::Data<LockState>) -> impl Responder {
     let lock_key = params.to_string();
     let is_acquire = lock_state.lock.is_acquire(&lock_key);
 
     HttpResponse::Ok().json(is_acquire)
 }
 
-pub async fn add_location() -> impl Responder {
+pub async fn location_put_handler() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn location_get_by_id_handler() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn location_get_nearby_handler() -> impl Responder {
     HttpResponse::Ok().finish()
 }
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,5 +1,5 @@
 use actix_web::web;
-use crate::handlers::{add_location, cache_delete_handler, cache_get_handler, cache_keys_get_handler, cache_put_handler, health_check_handler, lock_acquire, lock_release, lock_status};
+use crate::handlers::{cache_delete_handler, cache_get_handler, cache_keys_get_handler, cache_put_handler, health_check_handler, location_put_handler, lock_acquire_handler, lock_release_handler, lock_status_handler};
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
@@ -18,15 +18,15 @@ pub fn cache_routes(cfg: &mut web::ServiceConfig) {
 pub fn lock_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/lock")
-            .route("/{lock_key}/acquire", web::post().to(lock_acquire))
-            .route("/{lock_key}/release", web::post().to(lock_release))
-            .route("/{lock_key}", web::get().to(lock_status))
+            .route("/{lock_key}/acquire", web::post().to(lock_acquire_handler))
+            .route("/{lock_key}/release", web::post().to(lock_release_handler))
+            .route("/{lock_key}", web::get().to(lock_status_handler))
     );
 }
 
 pub fn geospatial_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/geospatial")
-            .route("/locations", web::post().to(add_location))
+            .route("/locations", web::post().to(location_put_handler()))
     );
 }
