9b4a572c186cc02cc913bc68cbbbb868763a7dfc
kurlendapawel <kurlendapawel@gmail.com>
2024-04-27 21:38:53 +0200

    add geospatial error

--- sdk/src/error.rs
+++ sdk/src/error.rs
@@ -1,11 +1,17 @@
+use thiserror::Error;
 
-
+#[derive(Debug, Error)]
 pub enum CafardError {
     ErrorType1 = 1,
     ErrorType = 2
 }
 
+#[derive(Debug, Error)]
 pub enum GeospatialError {
-    ErrorType1 = 1,
-    ErrorType2 = 2
+    #[error("Error")]
+    Error = 1,
+    #[error("Latitude must be between -90.0 and 90.0")]
+    LatitudeOutOfRange = 2,
+    #[error("Longitude must be between -180.0 and 180.0")]
+    LongitudeOutOfRange = 3
 }
