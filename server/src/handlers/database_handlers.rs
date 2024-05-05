db5ac441368737dd0d36a3c78a0c15835f8e8dc3
kurlendapawel <kurlendapawel@gmail.com>
2024-05-05 14:38:07 +0200

    add simple database not implemented handlers

---
+++ server/src/handlers/database_handlers.rs
@@ -0,0 +1,20 @@
+use actix_web::{HttpResponse, Responder, web};
+use crate::state::CacheState;
+
+pub async fn database_create_collection_handler() -> impl Responder {
+}
+
+pub async fn database_update_collection_handler() -> impl Responder {
+}
+
+pub async fn database_delete_collection_handler() -> impl Responder {
+}
+
+pub async fn database_create_collection_index_handler() -> impl Responder {
+}
+
+pub async fn database_update_collection_index_handler() -> impl Responder {
+}
+
+pub async fn database_delete_collection_index_handler() -> impl Responder {
+}
