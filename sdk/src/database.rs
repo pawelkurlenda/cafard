273313763c2ecaae2837457a15a51d4388d8c0d6
kurlendapawel <kurlendapawel@gmail.com>
2024-05-08 19:30:30 +0200

    remove sample functions descriptions

--- sdk/src/database.rs
+++ sdk/src/database.rs
@@ -1,16 +1,13 @@
 use std::collections::{HashMap, HashSet};
-use serde_json::{Value, json};
-use std::sync::{Arc, Mutex, RwLock};
-use crate::geospatial::Geospatial;
+use serde_json::Value;
+use std::sync::Arc;
 
-/// Represents a single document within the database.
 #[derive(Debug, Clone)]
 struct Document {
     id: u32,
-    data: Value,  // Using serde_json::Value to store arbitrary JSON data
+    data: Value,  // todo: Using serde_json::Value to store arbitrary JSON data
 }
 
-/// A collection of documents.
 #[derive(Debug, Clone)]
 struct Collection {
     name: String,
@@ -29,12 +26,10 @@ impl Collection {
         }
     }
 
-    /// Insert a new document into the collection.
     fn insert(&mut self, doc: Document) {
         self.documents.insert(doc.id, doc);
     }
 
-    /// Simple SELECT-like operation, searching by ID.
     fn select_by_id(&self, id: u32) -> Option<&Document> {
         self.documents.get(&id)
     }
@@ -48,7 +43,6 @@ impl Collection {
     }
 }
 
-/// The main database structure.
 #[derive(Debug, Clone)]
 pub struct Database {
     collections: HashMap<String, Collection>,
@@ -61,12 +55,10 @@ impl Database {
         })
     }
 
-    /// Add a new collection to the database.
     pub fn add_collection(&mut self, collection: Collection) {
         self.collections.insert(collection.name.clone(), collection);
     }
 
-    /// Get a collection by name.
     pub fn get_collection(&self, name: &str) -> Option<&Collection> {
         self.collections.get(name)
     }
