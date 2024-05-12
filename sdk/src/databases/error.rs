76e6ad6216df1ecfe209981bbd79ca1a53983b57
kurlendapawel <kurlendapawel@gmail.com>
2024-05-12 20:35:11 +0200

    add simple database errors

--- sdk/src/databases/error.rs
+++ sdk/src/databases/error.rs
@@ -3,4 +3,16 @@ use thiserror::Error;
 
 #[derive(Debug, Error, Serialize)]
 pub enum DatabaseError {
+    #[error("Error")]
+    Error = 1,
+    #[error("Database do not exists")]
+    DatabaseDoNotExists = 2,
+    #[error("Collection do not exists")]
+    CollectionDoNotExists = 3,
+    #[error("Index do not exists")]
+    IndexDoNotExists = 4,
+    #[error("Index already exists")]
+    IndexAlreadyExists = 5,
+    #[error("Unique index duplication")]
+    UniqueIndexDuplication = 6,
 }
