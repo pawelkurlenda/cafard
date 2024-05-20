b089017ac6044ea22f48d45ff1a8a228d6687f49
kurlendapawel <kurlendapawel@gmail.com>
2024-05-20 15:40:19 +0200

    add IndexFieldSchema constructor

--- sdk/src/databases/index.rs
+++ sdk/src/databases/index.rs
@@ -1,4 +1,7 @@
-
+use std::collections::HashMap;
+use std::sync::Mutex;
+use crate::databases::collection::Collection;
+use crate::databases::error::DatabaseError;
 
 #[derive(Debug)]
 pub struct IndexSchema {
@@ -17,4 +20,18 @@ pub struct IndexFieldSchema {
 pub enum IndexFieldOrder {
     ASC,
     DSC
+}
+
+impl IndexSchema {
+    fn new(fields: Vec<IndexFieldSchema>, is_unique: bool) -> Result<Self, DatabaseError> {
+        //fields.iter()
+        // todo: check for duplicates, consider to valid that earlier where fields existing is validate
+        // combine value for NAME
+
+        Ok(IndexSchema {
+            is_unique: is_unique,
+            fields: fields,
+            name: "".to_string()
+        })
+    }
 }
