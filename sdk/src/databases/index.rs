8642883421923305fa160fc3e955598703e98b95
kurlendapawel <kurlendapawel@gmail.com>
2024-05-20 14:35:15 +0200

    add IndexFieldSchema

--- sdk/src/databases/index.rs
+++ sdk/src/databases/index.rs
@@ -4,5 +4,17 @@
 pub struct IndexSchema {
     name: String,
     is_unique: bool,
-    //fields:
+    fields: Vec<IndexFieldSchema>
+}
+
+#[derive(Debug)]
+pub struct IndexFieldSchema {
+    field_name: String,
+    order: IndexFieldOrder
+}
+
+#[derive(Debug)]
+pub enum IndexFieldOrder {
+    ASC,
+    DSC
 }
