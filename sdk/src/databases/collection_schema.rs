5815a08b199ae8e23175d57c45bcb6b5a323b722
kurlendapawel <kurlendapawel@gmail.com>
2024-05-27 22:00:06 +0200

    CollectionSchema check_fields_availability add unit tests

--- sdk/src/databases/collection_schema.rs
+++ sdk/src/databases/collection_schema.rs
@@ -27,3 +27,63 @@ impl CollectionSchema {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup() -> CollectionSchema {
+        CollectionSchema {
+            fields: vec![
+                FieldSchema {
+                    name: "id".to_string(),
+                    is_primary_key: true,
+                    is_autoincrement: true,
+                    is_row_version: false,
+                    field_type: "int".to_string(),
+                },
+                FieldSchema {
+                    name: "username".to_string(),
+                    is_primary_key: false,
+                    is_autoincrement: false,
+                    is_row_version: false,
+                    field_type: "varchar".to_string(),
+                },
+                FieldSchema {
+                    name: "email".to_string(),
+                    is_primary_key: false,
+                    is_autoincrement: false,
+                    is_row_version: false,
+                    field_type: "varchar".to_string(),
+                },
+            ],
+        }
+    }
+
+    #[test]
+    fn test_successful_field_check() {
+        let schema = setup();
+        let fields_to_check = vec!["id", "username"];
+        assert!(schema.check_fields_availability(fields_to_check).is_ok());
+    }
+
+    #[test]
+    fn test_field_does_not_exist() {
+        let schema = setup();
+        let fields_to_check = vec!["id", "nonexistent"];
+        match schema.check_fields_availability(fields_to_check) {
+            Err(DatabaseError::FieldDoesNotExists) => assert!(true),
+            _ => assert!(false, "Field does not exist error not triggered."),
+        }
+    }
+
+    #[test]
+    fn test_duplicate_fields() {
+        let schema = setup();
+        let fields_to_check = vec!["id", "id"];
+        match schema.check_fields_availability(fields_to_check) {
+            Err(DatabaseError::IndexFieldDuplication) => assert!(true),
+            _ => assert!(false, "Duplicate fields error not triggered."),
+        }
+    }
+}
